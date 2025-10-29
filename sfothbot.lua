f58b44e8c575258864de32847ab37e9d24672e26
yixiyotatsuki <161025930+yixiyotatsuki@users.noreply.github.com>
2025-10-29 23:29:47 +0800

    Update sfothbot.lua

--- sfothbot.lua
+++ sfothbot.lua
@@ -67,7 +67,14 @@ task.spawn(function() -- this is the kill logs shit the anticheat is in the fixe
             end)
         end
     end
+    for i,v in pairs(Players:GetPlayers()) do
+        check(v)
+    end
+    Players.PlayerAdded:Connect(function(player)
+        check(player)
+    end)
 end)
+print("hitlog loaded")
 
 function funcs:GetSword() -- this is quite literally just a function from funclib but i replaced it with waitforchild
     local object=Player:WaitForChild("Backpack"):WaitForChild("Sword",.01)
@@ -92,7 +99,7 @@ function funcs:Hit(part,number)
 end
 
 
-RunService.FixedHeartbeat:Connect(function(dt)
+RunService.Heartbeat:Connect(function(dt) -- fixedheartbeat DIDNT WORK BECAUSE OF IDENTITY LEVELS AHAHAHAHAHAHAHA
     if settings.fling==nil then
         funcs:SetPosition(Vector3.new(0,-100000,0))
         for i,v in pairs(settings.killing) do
@@ -102,8 +109,11 @@ RunService.FixedHeartbeat:Connect(function(dt)
                 end
             end
             if v.Character:WaitForChild("Humanoid").Health==0 then table.remove(settings.killing,i) end
-        end        for i,v in pairs(settings.temp_loop) do
-            local player
+        end         
+        for i,v in pairs(settings.temp_loop) do
+            local player=Players:WaitForChild(v,dt*1.5)
+            if not player then continue end
+            funcs:Hit(player.Character.PrimaryPart,0)
         end
         for i,v in pairs(Players:GetPlayers()) do
             if table.find(whitelist,v.Name) then
@@ -131,6 +141,7 @@ RunService.FixedHeartbeat:Connect(function(dt)
         end
     end
 end)
+print("hb loop loaded")
 -- finaly commands!!!
 
 TextChatService.MessageReceived:Connect(function(msg)
@@ -167,111 +178,13 @@ TextChatService.MessageReceived:Connect(function(msg)
                 local player=funclib:GetPlayer(args[1])
                 settings.fun.instakill_sword=not settings.fun.instakill_sword
             elseif cmd=="whitelist" then
-                table.insert(settings.temp_whitelist,player.Name)
-            end
-        end
-    end
-end)
-
-print("script made by tatsuki (@.zamnsuki)!!! still lazy but atleast its better than before")
-
---[[
--- no ui lib this time
-
-local funclib=loadstring(game:HttpGet("https://raw.githubusercontent.com/yixiyotatsuki/stuff/main/funclib.lua"))()
-local Players=game:GetService("Players")
-local Owner=Players:FindFirstChild("Kandale_Dingul")
-local TextChatService=game:GetService("TextChatService")
-local RunService=game:GetService("RunService")
-local HB=RunService.Heartbeat
-local Player=Players.LocalPlayer
-local Character:Model=Player.Character
-local HumanoidRootPart:BasePart=Character:WaitForChild("HumanoidRootPart",9e9)
-Player.CharacterAdded:Connect(function(character)
-    workspace.FallenPartsDestroyHeight=0/0
-    Character=character
-    HumanoidRootPart=Character:WaitForChild("HumanoidRootPart",9e9)
-end)
-workspace.FallenPartsDestroyHeight=0/0
-if Owner==nil then return print("GANG OWNER DOESNT  EXIST ARE YOU FUCKING RETARD RETARD RETARD RETARD RETARD RETARD RETARD RETARD RETARD") end
-local funcs={}
-function funcs:GetSword() -- this is quite literally just a function from funclib but i replaced it with waitforchild
-    local object=Player:WaitForChild("Backpack"):WaitForChild("Sword",.01)
-    if object==nil and Player.Character~=nil then object=game:GetService("Players").LocalPlayer.Character:WaitForChild("Sword",.01) end
-    return object
-end
-	function cframe_but_its_not_cframe(PositionOfCourse:Vector3)
-		Character:TranslateBy(PositionOfCourse-HumanoidRootPart.Position)
-	end
-local flinging=false
-function fling(VeryCoolVariableName)
-	local layer_iaccidentallymispelledplayerbutidontcare=VeryCoolVariableName
-	local hrp,iusuallydontneedavariableforthisbutitsok=layer_iaccidentallymispelledplayerbutidontcare:FindFirstChild("HumanoidRootPart"),layer_iaccidentallymispelledplayerbutidontcare:FindFirstChild("Humanoid")
-	if not hrp or not iusuallydontneedavariableforthisbutitsok then return end
-	-- from now good variable names.. i think
-	local InitCFrame=HumanoidRootPart.CFrame
-	local time=0
-	flinging=true
-	repeat
-		local RetardPrediction=hrp.Velocity/(1.25+funclib:GetPing())
-		cframe_but_its_not_cframe(hrp.Position+RetardPrediction)
-		HumanoidRootPart.Velocity=Vector3.new(0,-100000,0)
-        HumanoidRootPart.RotVelocity=Vector3.new(0,1000,0)
-		time+=task.wait()
-	until not hrp:IsDescendantOf(game) or time>5
-	flinging=false
-end
-local looplist={"Guys_imtulimullinew","D_eadkreek","Yuri_Watanabe"}--(ChancedGRIM, reason: annoying), (also chancedgrim), also chancedgrim
-HB:Connect(function(deltaTime)
-    if not flinging then cframe_but_its_not_cframe(Vector3.new(0,-10000,0)) end
-    if not flinging then HumanoidRootPart.Velocity=Vector3.zero end
-    local sword=funcs:GetSword()
-    sword.Handle.Size=Vector3.new(20,20,20)
-    sword.Handle.Massless=true
-    if flinging then sword.Parent=Player.Backpack else Character:WaitForChild("Humanoid"):EquipTool(sword) end
-    for i,v in pairs(looplist) do
-        local player=Players:FindFirstChild(v)
-        if not player or not player.Character then continue end 
-        if player.Character:WaitForChild("Sword",deltaTime) then player.Character:WaitForChild("Sword",deltaTime):Destroy() end
-        player.Character:PivotTo(CFrame.new(sword.Handle.Position,player.Character:GetPivot().Position+(Vector3.new(math.random(-1,1),math.random(-1,1),math.random(-1,1))*5)))
-    end
-end)
-
-TextChatService.OnIncomingMessage=function(msg:TextChatMessage)
-    if msg.TextSource then
-        if string.match(msg.TextChannel.Name,"RBXWhisper") and msg.TextSource.UserId==Owner.UserId then
-            local msg=msg.Text
-            local args=msg:sub(2,#msg):split(" ")
-            local cmd=args[1]
-            table.remove(args,1)
-            if msg:sub(1,1)==";" then
-                if cmd=="kill" then
-                    local delta=task.wait()
-                    local v=funclib:GetPlayer(args[1],false)
-                    task.spawn(function()
-                        v.Character:BreakJoints()
-                        while v.Character:WaitForChild("Humanoid").Health>1 do
-                            if v.Character:WaitForChild("Sword",delta) then v.Character:WaitForChild("Sword",delta):Destroy() end
-                            v.Character:PivotTo(CFrame.new(funcs:GetSword().Handle.Position,v.Character:GetPivot().Position+(Vector3.new(math.random(-1,1),math.random(-1,1),math.random(-1,1))*5)))
-                        end
-                    end)
-                elseif cmd=="loop" then
-                    table.insert(looplist,args[1])
-                elseif cmd=="unloop" then
-                    local index=0
-                    for i,v in ipairs(looplist) do if v.Name==args[1] then index=i break end end
-                    table.remove(looplist,index)
-                elseif cmd=="fling" then
-                    local v=funclib:GetPlayer(args[1],false)
-                    fling(v.Character)
-				elseif cmd=="fakeflinging" then
-					flinging=not flinging
+                local player=funclib:GetPlayer(args[1])
+                if player then
+                    table.insert(settings.temp_whitelist,player.Name) 
                 end
             end
         end
     end
-end
-
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yixiyotatsuki/newhub/heads/main/sfothbot.lua"))()
-]]
+end)
+print('cmds loaded')
+print("script made by tatsuki (@.zamnsuki)!!! still lazy but atleast its better than before")
